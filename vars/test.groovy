a368420a4a409c413a1cad144b4cf342b504580b
mitun_msdian <43162712+mitunmbj@users.noreply.github.com>
2020-02-06 08:31:14 +0530

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -7,3 +7,14 @@ def call(Map stageParams) {
     ])
   }
 
+def call(String project, String hubUser) {
+    sh "docker build -t mitun.azurecr.io/ubuntu:latestone ."
+    withCredentials([usernamePassword(
+            credentialsId: "docker-registery",
+            usernameVariable: "USER",
+            passwordVariable: "PASS"
+    )]) {
+        sh "docker login -u mitun -p 5opvsXZVvPk94tCoasrBbUy4M0dVFFL/ mitun.azurecr.io"
+    }
+    sh "docker push mitun.azurecr.io/ubuntu:latestone"
+}
