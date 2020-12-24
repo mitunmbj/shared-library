8db64a1508ad650920ad623a6224eff53f9c2a34
mitun_msdian <43162712+mitunmbj@users.noreply.github.com>
2020-12-24 22:31:47 +0530

    Update gitCheckout.groovy

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -1,11 +1,11 @@
-def call(String url) {
+def call(String url branch) {
 pipeline {
     agent none
     stages {
         stage('Checkout') {
             agent { label 'docker_linux' }
             steps {
-              checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: "${url}"]]]) 
+              checkout([$class: 'GitSCM', branches: [[name: '*/"$branch"']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: "${url}"]]]) 
   
             }
            }
